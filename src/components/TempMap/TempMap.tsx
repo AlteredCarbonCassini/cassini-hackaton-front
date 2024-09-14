59d2e2a34514f373bf5e351efee1339903ca00db
Anton <a.chertok@akatech.com.ua>
2024-09-14 12:53:05 +0100

    Update TempMap.tsx

--- src/components/TempMap/TempMap.tsx
+++ src/components/TempMap/TempMap.tsx
@@ -25,6 +25,7 @@ const LAYERS_IDS = [
 ];
 
 function LocationMarker() {
+  // const [position] = useState<LatLngExpression>([51.505, -0.09]);
   const [position] = useState<LatLngExpression>([51.505, -0.09]);
 
   // const map = useMap();
@@ -66,8 +67,8 @@ const TempMap = () => {
   const baseUrl =
     "https://services.sentinel-hub.com/ogc/wms/3783c494-8b50-4792-941f-7897789fe94b";
 
-  // const customTemplateBaseUrl =
-  //   "https://creodias.sentinel-hub.com/ogc/wms/00922745-a285-4ee4-8691-2da608fb5a53";
+  const customTemplateBaseUrl =
+    "https://creodias.sentinel-hub.com/ogc/wms/00922745-a285-4ee4-8691-2da608fb5a53";
 
   const params = {
     time: "2023-09-01/2023-09-13",
@@ -96,12 +97,46 @@ const TempMap = () => {
           </LayersControl.Overlay>
         ))}
 
-        {/* <LayersControl.Overlay name="NITROGEN-DIOXIDE">
-          <SentinelLayer
-            layerID="NITROGEN-DIOXIDE"
-            url={customTemplateBaseUrl}
+        {/* <LayersControl.Overlay name="EW_HH_DB">
+          <SentinelLayer layerID="EW_HH_DB" url={customTemplateBaseUrl} />
+        </LayersControl.Overlay> */}
+
+        {/* <LayersControl.Overlay name="SO2">
+          <WMSTileLayer
+            layers="SO2"
+            format="image/jpeg"
+            attribution='&copy; <a href="http://www.sentinel-hub.com/" target="_blank">Sentinel Hub</a>'
+            url="https://creodias.sentinel-hub.com/ogc/wms/b1f19051-80f8-4545-b4e5-29a0a2148f3c"
+            minZoom={6}
+            maxZoom={16}
+            opacity={0.3}
           />
         </LayersControl.Overlay> */}
+
+        {/* <LayersControl.Overlay name="CH4">
+          <WMSTileLayer
+            layers="CH4"
+            format="image/jpeg"
+            attribution='&copy; <a href="http://www.sentinel-hub.com/" target="_blank">Sentinel Hub</a>'
+            url="https://creodias.sentinel-hub.com/ogc/wms/00922745-a285-4ee4-8691-2da608fb5a53"
+            minZoom={6}
+            maxZoom={16}
+            opacity={0.3}
+          />
+        </LayersControl.Overlay> */}
+
+        {/* <LayersControl.Overlay name="EW_HH_DB">
+          <WMSTileLayer
+            layers="EW_HH_DB"
+            format="image/jpeg"
+            attribution='&copy; <a href="http://www.sentinel-hub.com/" target="_blank">Sentinel Hub</a>'
+            url="https://services.sentinel-hub.com/ogc/wms/717540a4-e45e-4660-875e-052d4f409280"
+            minZoom={6}
+            maxZoom={16}
+            opacity={0.3}
+          />
+        </LayersControl.Overlay>
+      </LayersControl> */}
       </LayersControl>
 
       <LocationMarker />
