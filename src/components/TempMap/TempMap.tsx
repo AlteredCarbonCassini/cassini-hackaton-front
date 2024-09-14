7df97dba74befb6d03934ef36700d97084a399bc
Dmytro Bulakhov <bulakhovda@gmail.com>
2024-09-14 17:43:28 +0300

    feat: Added sent-2 and sent-3 layers

--- src/components/TempMap/TempMap.tsx
+++ src/components/TempMap/TempMap.tsx
@@ -23,6 +23,9 @@ import { LatLngExpression } from "leaflet";
 //   "SWIR",
 //   "TRUE-COLOR-S2L2A",
 // ];
+const LAYERS_IDS_SNT_2 = ["ULYSSYS-WATER-QUALITY-VIEWER"];
+
+const LAYERS_IDS_SNT_3 = ["OTCI"];
 
 const LAYERS_IDS_SNT_5 = [
   "AER-AI-340-AND-380",
@@ -69,15 +72,35 @@ const SentinelLayer = ({ layerID, url }: { layerID: string; url: string }) => (
     url={url}
     minZoom={6}
     maxZoom={16}
+  />
+);
+
+const SentinelLayerOpacity = ({
+  layerID,
+  url,
+}: {
+  layerID: string;
+  url: string;
+}) => (
+  <WMSTileLayer
+    layers={layerID}
+    format="image/jpeg"
+    attribution='&copy; <a href="http://www.sentinel-hub.com/" target="_blank">Sentinel Hub</a>'
+    url={url}
+    minZoom={6}
+    maxZoom={16}
     opacity={0.4}
   />
 );
 
 const TempMap = () => {
-  // const baseUrl =
-  //   "https://services.sentinel-hub.com/ogc/wms/3783c494-8b50-4792-941f-7897789fe94b";
+  const sent2_BaseUrl =
+    "https://services.sentinel-hub.com/ogc/wms/08853bf3-2fc8-4a35-aeeb-f2405ed1671e";
 
-  const customTemplateBaseUrl =
+  const sent3_BaseUrl =
+    "https://creodias.sentinel-hub.com/ogc/wms/b6614a1c-ec70-4fc5-a305-c7638caad1e8";
+
+  const sent5_BaseUrl =
     "https://creodias.sentinel-hub.com/ogc/wms/b1f19051-80f8-4545-b4e5-29a0a2148f3c";
 
   // const params = {
@@ -88,7 +111,7 @@ const TempMap = () => {
 
   // const url = baseUrl + "?" + searchParams;
 
-  const center: LatLngExpression = [52.2297, 21.0122]; // Варшава
+  const center: LatLngExpression = [52.2297, 21.0122]; // Warsaw
 
   return (
     <MapContainer
@@ -103,9 +126,21 @@ const TempMap = () => {
       />
 
       <LayersControl position="topright">
+        {LAYERS_IDS_SNT_2.map(id => (
+          <LayersControl.Overlay key={id} name={id}>
+            <SentinelLayer layerID={id} url={sent2_BaseUrl} />
+          </LayersControl.Overlay>
+        ))}
+
+        {LAYERS_IDS_SNT_3.map(id => (
+          <LayersControl.Overlay key={id} name={id}>
+            <SentinelLayerOpacity layerID={id} url={sent3_BaseUrl} />
+          </LayersControl.Overlay>
+        ))}
+
         {LAYERS_IDS_SNT_5.map(id => (
           <LayersControl.Overlay key={id} name={id}>
-            <SentinelLayer layerID={id} url={customTemplateBaseUrl} />
+            <SentinelLayerOpacity layerID={id} url={sent5_BaseUrl} />
           </LayersControl.Overlay>
         ))}
       </LayersControl>
