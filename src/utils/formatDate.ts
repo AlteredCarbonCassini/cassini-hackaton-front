83c3fa7beb216a188fb251922f7ab2eca44c8ae7
IraMira333 <irinat333@gmail.com>
2024-09-15 00:23:03 +0300

    Update formatDate.ts

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -4,7 +4,7 @@ export const formatDate = (date: Date) => {
     month: "2-digit",
     // year: "numeric",
     hour: "2-digit",
-    minute: "2-digit",
+    // minute: "2-digit",
     hour12: false,
   };
   return date.toLocaleString("en-GB", options as any).replace(",", "");
